f929f6b4bf8784c343e20f8c3d95974ac83010b7
Muhammad Khoirul Anwar <khoirula964@gmail.com>
2025-05-02 14:49:56 +0700

    add local auth

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,15 @@ const qrcode = require("qrcode-terminal");
 app.use(express.json());
 
 // Inisialisasi WhatsApp Client
+// const client = new Client({
+//     authStrategy: new LocalAuth(), // Menyimpan sesi login
+// });
 const client = new Client({
-    authStrategy: new LocalAuth(), // Menyimpan sesi login
+    authStrategy: new LocalAuth(),
+    webVersionCache: {
+        type: 'remote',
+        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+    }
 });
 
 client.on("qr", (qr) => {
