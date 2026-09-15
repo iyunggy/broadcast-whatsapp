2a93d76058fdbc9050d1945e059346e471778f5f
Muhammad Khoirul Anwar <khoirula964@gmail.com>
2026-09-15 16:17:06 +0700

    broadcast maleo

---
+++ broadcast-maleo/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+
+// Middleware untuk parsing JSON
+app.use(express.json());
+
+// Inisialisasi WhatsApp Client
+const client = new Client({
+    authStrategy: new LocalAuth(), // Menyimpan sesi login
+});
+
+client.on("qr", (qr) => {
+    console.log("Scan QR Code ini untuk login:");
+    qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+    console.log("WhatsApp bot siap!");
+});
+
+client.initialize();
+
+// Ekspor client agar bisa digunakan di routes
+module.exports.client = client;
+
+// routes
+const broadcastWhatsappRoutes= require('./routes/broadcastWhatsapp')
+
+// Route sederhana
+app.get("/", (req, res) => {
+    res.send("Hello, Express!");
+});
+
+app.use('/broadcastwhatsapp', broadcastWhatsappRoutes)
+
+// Jalankan server
+app.listen(port, () => {
+    console.log(`Server berjalan di http://localhost:${port}`);
+});
