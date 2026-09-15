6224634a15d570f339f0a1129a7d46e8ca87c8fe
Muhammad Khoirul Anwar <khoirula964@gmail.com>
2026-09-15 16:32:25 +0700

    broadcast maleo

--- broadcast-maleo/server.js
+++ broadcast-maleo/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 
